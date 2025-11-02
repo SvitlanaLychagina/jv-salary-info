0142ae241f74cfd4545949eec7d5e2f0238973b6
Svitlana Lychahina <svetlana.lychagina02@gmail.com>
2025-11-02 11:47:24 +0200

    Implemented method getSalaryInfo() to calculate the salary for employees

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,39 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    private static final DateTimeFormatter FORMATTER =
+            DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final int DATE_POSITION = 0;
+    private static final int NAME_POSITION = 1;
+    private static final int HOUR_POSITION = 2;
+    private static final int INCOME_POSITION = 3;
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        StringBuilder report = new StringBuilder("Report for period "
+                + dateFrom + " - " + dateTo);
+        LocalDate firstDate = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate secondDate = LocalDate.parse(dateTo, FORMATTER);
+        String[] splitData;
+        for (String name : names) {
+            int fullSalary = 0;
+            for (String dataByName : data) {
+                splitData = dataByName.split(" ");
+                LocalDate actualDate = LocalDate.parse(splitData[DATE_POSITION], FORMATTER);
+                if (!actualDate.isAfter(secondDate) && !actualDate.isBefore(firstDate)
+                        && name.equals(splitData[NAME_POSITION])) {
+                    try {
+                        fullSalary += Integer.parseInt(splitData[HOUR_POSITION])
+                                * Integer.parseInt(splitData[INCOME_POSITION]);
+                    } catch (NumberFormatException e) {
+                        throw new NumberFormatException();
+                    }
+                }
+            }
+            report.append("\r\n").append(name).append(" - ").append(fullSalary);
+        }
+        return report.toString();
     }
 }
